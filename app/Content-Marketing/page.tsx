658dc855de53809df08848fce1fc758dcc247491
faheem221 <faheemk793@gmail.com>
2025-10-07 15:47:28 +0530

    done

--- app/Content-Marketing/page.tsx
+++ app/Content-Marketing/page.tsx
@@ -437,25 +437,64 @@ const Page = () => {
           </div>
         </div>
       </div>
-      <div className="py-16 container">
-        <div className=" ">
-          <div className=" p-7  w-full bg-[#b53f3f] text-slate-100 rounded-xl">
-            <h2 className="text-3xl font-SplineSans font-[500]">
+
+      <section className="py-20  ">
+        <div className="container rounded-lg border border-red-200 shadow max-w-5xl py-20 relative overflow-hidden">
+          <div className="absolute inset-0 ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              version="1.1"
+              viewBox="0 0 800 800"
+            >
+              <defs>
+                <radialGradient id="cccircular-grad" r="50%" cx="50%" cy="50%">
+                  <stop
+                    offset="15%"
+                    stopColor="#c95567"
+                    stopOpacity="0.5"
+                  ></stop>
+                  <stop
+                    offset="75%"
+                    stopColor="hsl(352, 100%, 76%)"
+                    stopOpacity="1"
+                  ></stop>
+                  <stop
+                    offset="100%"
+                    stopColor="#ffeaf8"
+                    stopOpacity="1"
+                  ></stop>
+                </radialGradient>
+              </defs>
+              <g fill="url(#cccircular-grad)">
+                <circle r="672" cx="400" cy="400" opacity="0.05"></circle>
+                <circle r="560" cx="400" cy="400" opacity="0.24"></circle>
+                <circle r="448" cx="400" cy="400" opacity="0.43"></circle>
+                <circle r="336" cx="400" cy="400" opacity="0.62"></circle>
+                <circle r="224" cx="400" cy="400" opacity="0.81"></circle>
+              </g>
+            </svg>
+          </div>
+          <div className="relative z-10">
+            <h1 className="text-3xl font-[600] font-Grostek text-center lg:text-5xl">
               Let’s Grow Together
-            </h2>
-            <p className="mt-3 font-Synonym font-[400] text-lg">
+            </h1>
+            <p className="text-center mt-2">
               Content marketing isn`t just about creating content—it`s about
               creating connections. Ready to elevate your brand? Partner with
               Spok Digital, where your story meets strategy.
             </p>
-            <Link href={"/contact"}>
-              <button className="mt-5 px-4 py-2 bg-white text-slate-900 font-SplineSans rounded-lg font-[400]">
-                Contact Us
-              </button>
-            </Link>
+            <div className="flex justify-center items-center gap-5 mt-5">
+              <a
+                href={"https://calendly.com/spokdigital/30min"}
+                className="bg-black px-4 py-[.4rem] rounded-lg text-slate-50"
+              >
+                {" "}
+                Book a Call Now
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </motion.div>
   );
 };
