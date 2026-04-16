2fe596a5c1e44452e90c62e19c3486a8838fffd9
Faheem <faheemk793@gmail.com>
2026-04-16 10:53:48 +0530

    fixed

--- app/(main)/(Home Page)/Text-Marquee.tsx
+++ app/(main)/(Home Page)/Text-Marquee.tsx
@@ -2,13 +2,13 @@ import CurvedLoop from "../App chunks/components/curvedMarquee";
 
 export function TextMarquee() {
   return (
-    <div className="relative mt-44 mb-24 flex w-full  h-[200px]  flex-col items-center justify-center">
+    <div className="relative mt-10 mb-12 lg:mt-44 lg:mb-24 flex w-full  h-[200px]  flex-col items-center justify-center">
       <CurvedLoop
         marqueeText="✦ WooCommerce ✦ ROAS ✦ Scaling ✦ Funnels ✦ Profit ✦ Shopify ✦ Conversions"
         speed={2}
         curveAmount={400}
         interactive={false}
-        className=" !fill-black text-7xl font-Grostek"
+        className=" !fill-black !text-[10rem] lg:text-7xl font-Grostek"
       />
     </div>
   );
